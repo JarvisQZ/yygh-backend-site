ed08ec4c4c8c2917243c65f9225a63d94d137076
ZHI <891023883@qq.com>
2022-02-20 21:38:32 +0800

    Initial commit

---
+++ src/views/table/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="app-container">
+        医院设置列表
+    </div>
+</template>
+
+<script>
+import { getList } from '@/api/table'
+
+export default {
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        published: 'success',
+        draft: 'gray',
+        deleted: 'danger'
+      }
+      return statusMap[status]
+    }
+  },
+  data() {
+    return {
+      list: null,
+      listLoading: true
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.listLoading = true
+      getList(this.listQuery).then(response => {
+        this.list = response.data.items
+        this.listLoading = false
+      })
+    }
+  }
+}
+</script>
